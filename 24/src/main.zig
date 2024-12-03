6e5a9f6ca48ae008da2523abd3820cd7499a9aec
Mitch <68300083+mitchharvey-dsg@users.noreply.github.com>
2024-12-02 21:38:37 -0500

    2b works

--- 24/src/main.zig
+++ 24/src/main.zig
@@ -1,74 +1,108 @@
 const std = @import("std");
+const print = std.debug.print;
+
+pub fn remove(comptime T: type, list: std.ArrayList(T), removeIdx: usize) anyerror!std.ArrayList(T) {
+    print("Removing index {d} ({d}) from {any}\n", .{ removeIdx, list.items[removeIdx], list.items });
+
+    var newList = std.ArrayList(i32).init(std.heap.c_allocator);
+    for (list.items[0..removeIdx]) |item| {
+        try newList.append(item);
+    }
+    for (list.items[removeIdx + 1 ..]) |item| {
+        try newList.append(item);
+    }
+
+    print("Final list: {any}\n", .{newList.items});
+    return newList;
+}
+
+pub fn seqSafe(lineList: std.ArrayList(i32)) anyerror!bool {
+    // print("Evaluating {any}\n", .{lineList.items});
+
+    const increasing = lineList.items[0] < lineList.items[1];
+
+    for (lineList.items[1..], 1..) |curr, i| {
+        const last = lineList.items[i - 1];
+        const diff: i32 = curr - last;
+        // print("Comparing {d} to {d}: ", .{ last, curr });
+
+        if (increasing and diff < 0) {
+            // print("Unsafe because increasing but found decreasing\n", .{});
+            return false;
+        }
+
+        if (!increasing and diff > 0) {
+            // print("Unsafe because decreasing but found increasing\n", .{});
+            return false;
+        }
+
+        const adiff = @abs(diff);
+        if (adiff < 1 or adiff > 3) {
+            // print("Unsafe because gap of {d} < 1 or {d} > 3\n", .{ adiff, adiff });
+            return false;
+        }
+    }
+    return true;
+}
 
 pub fn main() !void {
     var safe: u32 = 0;
-    var unsafe: u32 = 0;
+    var n: u32 = 0;
 
-    var file = try std.fs.cwd().openFile("../2/eginput", .{});
+    var file = try std.fs.cwd().openFile("../2/input", .{});
     defer file.close();
     var buf_reader = std.io.bufferedReader(file.reader());
     var in_stream = buf_reader.reader();
     var buf: [1024]u8 = undefined;
-    const stdout = std.io.getStdOut().writer();
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        try stdout.print("{s}\n", .{line});
+        n += 1;
+        // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+        // const allocator = gpa.allocator();
+        // var lineList = std.ArrayList(u32).init(allocator);
+        var lineList = std.ArrayList(i32).init(std.heap.c_allocator);
+        defer lineList.deinit();
+        var lineIterator = std.mem.splitScalar(u8, line, ' ');
+        while (lineIterator.next()) |x| {
+            const parsed = try std.fmt.parseInt(i32, x, 10);
+            try lineList.append(parsed);
+        }
 
-        var it = std.mem.splitScalar(u8, line, ' ');
-        // split to array
-        var last: i32 = -1;
-        var curr: i32 = -1;
-        var increasing = true;
-        var unsafecount: u32 = 0;
         // iterate through array, if unsafe, try removing that level and retry
         // if still unsafe, then unsafe, else safe
-        while (it.next()) |x| {
-            const fixedx = try std.fmt.parseInt(i32, x, 10);
-            if (last == -1) {
-                last = fixedx;
-                continue;
-            }
-            if (curr == -1) {
-                if (last < fixedx) {
-                    increasing = true;
-                } else {
-                    increasing = false;
-                }
-            }
 
-            curr = fixedx;
-            const diff = curr - last;
-            std.debug.print("Comparing {d} to {d}: ", .{ last, curr });
-            last = curr;
-
-            if (increasing and diff < 0) {
-                std.debug.print("Unsafe because increasing but found decreasing {d}\n", .{unsafecount});
-                unsafecount += 1;
-                continue;
-            }
-
-            if (!increasing and diff > 0) {
-                std.debug.print("Unsafe because decreasing but found increasing {d}\n", .{unsafecount});
-                unsafecount += 1;
-                continue;
-            }
-
-            const adiff = @abs(diff);
-            if (adiff < 1 or adiff > 3) {
-                std.debug.print("Unsafe because gap of {d} < 1 or > 3, {d}\n", .{adiff, unsafecount});
-                unsafecount += 1;
-                continue;
-            }
-
-            std.debug.print("safe! {d} {d} {d}\n", .{ diff, adiff, unsafecount});
-        }
-        if (unsafecount <= 1) {
-            std.debug.print("Sequence safe!\n", .{});
+        // if (try seqSafe(lineList, true)) {
+        //     print("Sequence safe!\n", .{});
+        //     safe += 1;
+        // } else {
+        //     const newList = try remove(i32, lineList, lineList.items.len-1); //lazy
+        //     if (try seqSafe(newList, false)) {
+        //         print("Sequence safe after removing end!\n", .{});
+        //         safe += 1;
+        //     } else {
+        //         print("Sequence UNsafe!\n", .{});
+        //         unsafe += 1;
+        //     }
+        // }
+        if (try seqSafe(lineList)) {
+            // print("Sequence safe first try!\n", .{});
             safe += 1;
         } else {
-            std.debug.print("Sequence UNsafe!\n", .{});
-            unsafe += 1;
+            // var foundsafe = false;
+            for (0..lineList.items.len) |i| {
+                const newList = try remove(i32, lineList, i);
+                if (try seqSafe(newList)) {
+                    print("Sequence safe after removing! {d} {any} => {any}\n", .{ i, lineList.items, newList.items });
+                    // foundsafe = true;
+                    safe += 1;
+                    break;
+                }
+            }
+            // if (!foundsafe) {
+            //     print("Sequence unsafe even after removing! {any}\n", .{lineList.items});
+            //     unsafe += 1;
+            // }
         }
-        std.debug.print("CURR: UNSAFE: {d}\tSAFE: {d}\n", .{ unsafe, safe });
+        print("{d} CURR:\tSAFE: {d}\n", .{ n, safe });
     }
-    std.debug.print("UNSAFE: {d}\tSAFE: {d}\n", .{ unsafe, safe });
+    print("\tSAFE: {d}\n", .{safe});
 }
